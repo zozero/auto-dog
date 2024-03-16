6a7d95da015c83deb5a276f05842361f3af906b9
zozero <zoneonull@gmail.com>
2024-03-16 16:55:49 +0800

    添加范围截图的功能

--- src/app/image-process/image-process.component.ts
+++ src/app/image-process/image-process.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ButtonModule } from 'ng-devui/button';
 import { LayoutModule } from 'ng-devui';
@@ -10,6 +10,8 @@ import { ExecutionSideHttpService } from '../core/services/https/execution-side-
 import { CommonModule } from '@angular/common';
 import { MyLocalStorageService } from '../core/services/my-local-storage/my-local-storage.service';
 import { projectTable } from '../core/services/dexie-db/project-table.service';
+// Import angular-cropperjs
+import { AngularCropperjsModule, CropperComponent } from 'angular-cropperjs';
 
 @Component({
   selector: 'app-image-process',
@@ -22,13 +24,24 @@ import { projectTable } from '../core/services/dexie-db/project-table.service';
     SelectModule,
     FormsModule,
     SubMenusComponent,
-    CommonModule
+    CommonModule,
+    AngularCropperjsModule,
   ],
 })
 export class ImageProcessComponent implements OnInit {
   currentSubMenu!: ProjectInfo;
   showLoading = false;
+  cropConfig={
+    // 双击后可以拖动图片，再次双击后恢复
+    // toggleDragModeOnDblclick:true
+    // 根据百分比设置初始截取范围框，根据图片的大小
+    autoCropArea:0.2
+  }
   imageToShow: any;
+  cropImageData:any;
+
+  // Get with @ViewChild
+  @ViewChild('angularCropper') public angularCropper!: CropperComponent;
 
   constructor(
     private route: ActivatedRoute,
@@ -39,16 +52,18 @@ export class ImageProcessComponent implements OnInit {
     void this.initCurrentSubMenu();
   }
 
-  async initCurrentSubMenu(){
+  async initCurrentSubMenu() {
     // 获取已保存的菜单
     const curMuen = this.myLocalStorage.get('currentSubMenu');
-    console.log("🚀 ~ SubMenusComponent ~ getMenus ~ curMuen:", curMuen)
-    if(curMuen){
+    console.log('🚀 ~ SubMenusComponent ~ getMenus ~ curMuen:', curMuen);
+    if (curMuen) {
       this.currentSubMenu = await projectTable.queryProjectInfoByName(curMuen);
-      console.log("🚀 ~ SubMenusComponent ~ getMenus ~ this.currentSubMenu:", this.currentSubMenu)
-    }
-    else{
-      this.currentSubMenu =await projectTable.queryProjectFirstInfo();
+      console.log(
+        '🚀 ~ SubMenusComponent ~ getMenus ~ this.currentSubMenu:',
+        this.currentSubMenu
+      );
+    } else {
+      this.currentSubMenu = await projectTable.queryProjectFirstInfo();
     }
   }
 
@@ -60,14 +75,14 @@ export class ImageProcessComponent implements OnInit {
         this.currentSubMenu.simulatorInfo.ipPort
       )
       .subscribe((img) => {
-        console.log("🚀 ~ ImageProcessComponent ~ .subscribe ~ img:", img)
-        
+        console.log('🚀 ~ ImageProcessComponent ~ .subscribe ~ img:', img);
+
         const reader = new FileReader();
         reader.addEventListener(
           'load',
           () => {
             this.imageToShow = reader.result;
-            console.log("🚀 ~ ImageProcessComponent ~ .subscribe ~  this.imageToShow:",  this.imageToShow)
+            // console.log("🚀 ~ ImageProcessComponent ~ .subscribe ~  this.imageToShow:",  this.imageToShow)
             this.showLoading = false;
           },
           false
@@ -90,7 +105,26 @@ export class ImageProcessComponent implements OnInit {
     this.currentSubMenu = currentSubMenu;
   }
 
-  turnGray(){
-    
+  cropImage() {
+    this.angularCropper.exportCanvas();
+  }
+  angularCropperExport(data: any) {
+    console.log('🚀 ~ ImageProcessComponent ~ test1 ~ data:', data);
+    this.cropImageData=this.angularCropper.cropper.getCroppedCanvas().toDataURL('image/jpeg');
+    this.getCropImageInfo();
+  }
+  getCropImageInfo() {
+    // 获取当前图片的尺寸(可能被缩小了)
+    // this.angularCropper.cropper.getImageData()
+    // 获得当前图片显示区域的范围，包括离整个画板的距离
+    // this.angularCropper.cropper.getCanvasData()z
+    // 获得当前截取的图片的位置，其中x，y是原始大小的图片左上角开始的位置，width，height是截取的图片实际的大小
+    // this.angularCropper.cropper.getData()
+    const cropImageInfo=this.angularCropper.cropper.getData()
+    console.log(cropImageInfo);
+  }
+
+  resetCanvasImage(){
+    this.angularCropper.cropper.reset()
   }
 }
